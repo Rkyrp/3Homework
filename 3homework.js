98ad0c7284801467447f625c6ada25cab79ab228
Rkyrp <kyrpychenko.r@gmail.com>
2024-10-11 12:02:28 +0300

    3homework

--- 3homework.js
+++ 3homework.js
@@ -2,17 +2,17 @@ let year = 2000;
 console.log(year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0) ? 'высокосный': 'не высокосный');
 
 let number = 15;
-if (number % 3 == 0 && number % 5 == 0){
-    console.log('FizzleBuzz');
+switch (true) {
+    case (number % 3 == 0 && number % 5 == 0):
+        console.log("FizzBuzz");
+        break;
+    case (number % 3 == 0):
+        console.log("Fizz");
+        break;
+    case (number % 5 == 0):
+        console.log("Buzz");
+        break;
+    default:
+        console.log(number);
 }
-    else if (number % 3 == 0){
-        console.log('Fizzle');
-    }
-       else if (number % 5 == 0){
-            //console.log('Buzz');
-        }
-    else {
-        console.log('Wrong');
-    }
 
-   
